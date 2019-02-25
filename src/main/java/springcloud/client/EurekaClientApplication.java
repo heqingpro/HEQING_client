44288c73c1c31fa241a0846af219278d29024424
yundanfeng <1911620005@qq.com>
2019-02-25 17:36:44 +0800

    增加注册服务器自动刷新功能

--- src/main/java/springcloud/client/EurekaClientApplication.java
+++ src/main/java/springcloud/client/EurekaClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 @EnableEurekaClient
 @RestController
+@RefreshScope
 public class EurekaClientApplication {
     public static void main(String[] args){
         SpringApplication.run(EurekaClientApplication.class,args);
